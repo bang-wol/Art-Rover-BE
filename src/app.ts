173aaba0bf0789bfaf1409c30efcf9c3177c40af
bang-wol <102708198+bang-wol@users.noreply.github.com>
2024-07-10 04:15:08 +0900

    feat: 책 상세 검색 기능 구현
    - 책 검색 결과에서 특정 책을 클릭하면, 해당 책의 ISBN을 파라미터로 사용하여 상세 정보를 보여줌

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import searchBookRoutes from './routes/searchBookRoutes';
+import bookDetailRoutes from './routes/bookDetailRoutes'
 
 dotenv.config();
 
 const app = express();
 
 app.use('/search-books', searchBookRoutes);
+app.use('/book', bookDetailRoutes);
 
 export default app;
---
+++ src/controllers/bookDetailController.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import { RequestHandler } from 'express';
+import { StatusCodes } from 'http-status-codes';
+
+export const bookDetail: RequestHandler = async(req, res, next) => {
+    const { isbn } = req.params;
+    try{
+        const response = await axios.get('https://openapi.naver.com/v1/search/book.json', {
+            params: { query: isbn },
+            headers: {
+                'X-Naver-Client-Id': process.env.NAVER_CLIENT_ID,
+                'X-Naver-Client-Secret': process.env.NAVER_CLIENT_SECRET,
+            }
+        });
+        const bookDetails = response.data.items[0] || null;
+        if (!bookDetails) {
+            return res.status(StatusCodes.NOT_FOUND).json({ message: 'Book not found' })
+        }
+        res.json(bookDetails);
+    } catch(error){
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'Error fetching data from Naver API' })
+    }
+};
