60ce93befe09e5bb39e7a17fdde4ac05197b4376
bang-wol <102708198+bang-wol@users.noreply.github.com>
2024-07-13 05:44:28 +0900

    feat: MongoDB 연결
    Mongoose 사용을 위해 MongoDB와의 연결 설정

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import searchBookRoutes from './routes/searchBookRoutes';
 import bookDetailRoutes from './routes/bookDetailRoutes'
+import mongoose from "mongoose";
 
 dotenv.config();
 
@@ -9,5 +10,10 @@ const app = express();
 
 app.use('/search-books', searchBookRoutes);
 app.use('/book', bookDetailRoutes);
+const db: string = process.env.MONGODB_CONNECTION || "";
 
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB connected"))
+  .catch((error) => console.log(error));
 export default app;
