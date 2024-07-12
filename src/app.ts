d9a0d4a74bd095fd79485784abea35cbd67d065e
bang-wol <102708198+bang-wol@users.noreply.github.com>
2024-07-13 06:06:09 +0900

    fix: API 경로 설정 변경
    - 전체 검색, 상세 검색 경로 따로 설정했던 것을 하나로 합치게 되면서 경로를 '/api/books/search'로 변경

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,17 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import searchBookRoutes from './routes/searchBookRoutes';
-import bookDetailRoutes from './routes/bookDetailRoutes'
 import mongoose from "mongoose";
+import searchBookRoutes from "./routes/searchBooks";
 
 dotenv.config();
 
 const app = express();
 
-app.use('/search-books', searchBookRoutes);
-app.use('/book', bookDetailRoutes);
 const db: string = process.env.MONGODB_CONNECTION || "";
 
 mongoose
   .connect(db)
   .then(() => console.log("MongoDB connected"))
   .catch((error) => console.log(error));
+app.use("/api/books/search", searchBookRoutes);
 export default app;
