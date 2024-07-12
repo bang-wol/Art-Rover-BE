96ed59f4e281bc3aad38502aec1ec162b8ed5fe3
bang-wol <102708198+bang-wol@users.noreply.github.com>
2024-07-13 05:28:11 +0900

    refactor: 책 검색 기능 한 파일로 합침
    - 코드 구조 개선을 위해 관련 기능 하나의 파일로 합침

--- src/controllers/searchBookController.ts
+++ src/controllers/searchBookController.ts
@@ -1,19 +1,51 @@
-import axios from 'axios';
-import { RequestHandler } from 'express';
-import { StatusCodes } from 'http-status-codes';
+import axios from "axios";
+import { RequestHandler } from "express";
+import { StatusCodes } from "http-status-codes";
 
 export const searchBooks: RequestHandler = async (req, res, next) => {
-    const { query } = req.query;
-    try{
-        const response = await axios.get('https://openapi.naver.com/v1/search/book.json', {
-            params: { query },
-            headers: {
-                'X-Naver-Client-Id': process.env.NAVER_CLIENT_ID,
-                'X-Naver-Client-Secret': process.env.NAVER_CLIENT_SECRET,
-            }
-        });
-        res.json(response.data);
-    } catch(error) {
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'Error fetching data from Naver API' })
+  const { query } = req.query;
+  try {
+    const response = await axios.get(
+      "https://openapi.naver.com/v1/search/book.json",
+      {
+        params: { query },
+        headers: {
+          "X-Naver-Client-Id": process.env.NAVER_CLIENT_ID,
+          "X-Naver-Client-Secret": process.env.NAVER_CLIENT_SECRET,
+        },
+      }
+    );
+    res.json(response.data.items);
+  } catch (error) {
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: "Error fetching data from Naver API" });
+  }
+};
+
+export const searchBookDetail: RequestHandler = async (req, res, next) => {
+  const { isbn } = req.params;
+  try {
+    const response = await axios.get(
+      "https://openapi.naver.com/v1/search/book.json",
+      {
+        params: { query: isbn },
+        headers: {
+          "X-Naver-Client-Id": process.env.NAVER_CLIENT_ID,
+          "X-Naver-Client-Secret": process.env.NAVER_CLIENT_SECRET,
+        },
+      }
+    );
+    const bookDetails = response.data.items[0] || null;
+    if (!bookDetails) {
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ message: "Book not found" });
     }
+    res.json(bookDetails);
+  } catch (error) {
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: "Error fetching data from Naver API" });
+  }
 };
